2ec2726d68077ddcd8c13f8e3f5ab05fb72f7ffd
Garrett Wren <gwren19@gmail.com>
2020-06-06 16:22:29 -0700

    did some work to routine_activities db and api

--- api/routine_activities.js
+++ api/routine_activities.js
@@ -1,11 +1,48 @@
 const routinesActivitiesRouter = require('express').Router();
+const { getRoutineActivityById, updateRoutineActivity } = require('../db/routine_activities');
+const { requireUser } = require('./utils');
 
-routinesActivitiesRouter.patch(() => {
+routinesActivitiesRouter.patch('/:routineActivityId', requireUser, async (req, res, next) => {
+    const { routineActivityId } = req.params;
+    const { count, duration } = req.body;
 
+    const updateFields = {};
+
+    if (count) {
+        updateFields.count = count;
+    }
+
+    if (duration) {
+        updateFields.duration = duration;
+    }  
+
+    try {
+        const originalRoutineActivity = await getRoutineActivityById(routineActivityId);
+
+        if(originalRoutineActivity.creatorId === req.user.id) {
+            const updatedRoutineActivity = await updateRoutineActivity(routineActivityId, updateFields);
+            res.send({ routine: updatedRoutineActivity })
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    };
 });
 
-routinesActivitiesRouter.delete(() => {
+routinesActivitiesRouter.delete('/:routineActivityId', requireUser, async (req, res, next) => {
+    const { routineActivityId } = req.params;
 
+    try {
+        const routineActivity = await getRoutineActivityById(routineActivityId);
+
+        if (routineActivity.creatorId === req.user.id) {
+            await deleteRoutine(routineActivityId);
+            console.log('activity has been deleted from routine!');
+        }
+
+        res.send('activity has been deleted from routine!')
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
 });
 
 module.exports = routinesActivitiesRouter;
--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,5 +1,38 @@
 const client = require('./database');
 
+async function getRoutineActivityById(routineActivityId) {
+    try {
+        const { rows: [ routineActivity ] } = await client.query(`
+            SELECT *
+            FROM routine_activities
+            WHERE id=$1;
+        `, [ routineActivityId ]);
+
+        return routineActivity;
+    } catch (error) {
+        throw error;
+    }
+};
+
+async function updateRoutineActivity(id, fields = {}) {
+    const setString = Object.keys(fields).map(
+        (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+    if (setString.length === 0) {
+        return;
+    };
+    try {
+        const { rows } = await client.query(`
+            UPDATE users
+            SET ${ setString }
+            WHERE id=${ id }
+            RETURNING *;
+        `, Object.values(fields));
+        return rows;
+    } catch (error) {
+        throw error;
+    };
+};
 
 async function addActivityToRoutine({ routineId, activityId, count, duration}) {
     console.log('Adding activity to routine is being called!')
@@ -17,6 +50,16 @@ async function addActivityToRoutine({ routineId, activityId, count, duration}) {
     }
 };
 
+async function deleteActivityFromRoutine(id) {
+    await client.query(`
+        DELETE FROM routine_activities
+        WHERE "routineId"=$1;
+    `, [ id ])
+}
+
 module.exports = {
     addActivityToRoutine,
+    getRoutineActivityById,
+    updateRoutineActivity,
+    deleteActivityFromRoutine
 }
